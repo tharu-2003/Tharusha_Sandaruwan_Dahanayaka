a83cd98495c6835fb7b827508969735e2db7eb6f
tharu-2003 <sandaruwantharusha968@gmail.com>
2026-02-11 20:26:34 +0530

    add motion tags

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' 
 import './index.css'
 import App from './App.tsx'
+import { MotionConfig, motion } from 'framer-motion';
+
+import { AppProvider } from './context/AppContext.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-    <BrowserRouter> 
-      <App />
-    </BrowserRouter>
+    <BrowserRouter>
+      <AppProvider>
+        <MotionConfig>
+          <motion.div viewport={{ once: true }}>
+            <App />
+          </motion.div>
+        </MotionConfig>
+      </AppProvider>
+    </BrowserRouter>,
   // </StrictMode>,
 )
