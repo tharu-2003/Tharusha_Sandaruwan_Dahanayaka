20654a1b0605e93ab10f133d9c917c89ad39d1d8
tharu-2003 <sandaruwantharusha968@gmail.com>
2026-02-11 20:27:02 +0530

    add routes for new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import ThoughtsPage from "./pages/ThoughtsPage";
 import ProjectPage from "./pages/ProjectPage";
 import ExperiencePage from "./pages/ExperiencePage";
 import ToolPage from "./pages/ToolPage";
 import ContactPage from "./pages/ContactPage";
+import ContentsPage from "./pages/ContentsPage";
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";
+import ExperienceDetailsPage from "./pages/ExperienceDetailsPage";
 
 
 const App = () => {
@@ -14,9 +16,11 @@ const App = () => {
       <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/projects' element={<ProjectPage />} />
+            <Route path='/projects-details/:id' element={<ProjectDetailsPage />} />
             <Route path='/experience' element={<ExperiencePage />} />
+            <Route path='/experience-details/:id' element={<ExperienceDetailsPage />} />
             <Route path='/tools' element={<ToolPage />} />
-            <Route path='/thoughts' element={<ThoughtsPage />} />
+            <Route path='/contents' element={<ContentsPage />} />
             <Route path='/contact' element={<ContactPage />} />
       </Routes> 
     </>      
