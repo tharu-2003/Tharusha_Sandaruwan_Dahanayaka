b1774a75fbba456a52ef65a73153d3722f7d1e6d
tharu-2003 <sandaruwantharusha968@gmail.com>
2026-02-12 15:21:45 +0530

    add app context

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useContext } from "react";
+import axios from "axios";
+// import { toast } from 'react-hot-toast';
+import { useNavigate } from "react-router-dom";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+
+interface IAppContext {
+  navigate: ReturnType<typeof useNavigate>;
+  currency: string;
+  axios: typeof axios;
+}
+
+const AppContext = createContext<IAppContext | undefined>(undefined);
+
+export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+
+  const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY;
+
+
+  const value: IAppContext = {
+    navigate,
+    currency,
+    axios
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = (): IAppContext => {
+  const context = useContext(AppContext);
+  if (!context) throw new Error("useAppContext must be used within AppProvider");
+  return context;
+};
