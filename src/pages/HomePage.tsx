d3dde7e53ba11679321794a0cf7687d402cad1c8
tharu-2003 <sandaruwantharusha968@gmail.com>
2026-02-14 22:18:48 +0530

    responsive added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -14,27 +14,27 @@ const HomePage = () => {
       <Navigation />
       
       {/* Main Container */}
-      <div className="pt-20 md:pt-24">
+      <div className="pt-16 sm:pt-20 md:pt-24">
         {/* Mobile: Profile card centered at top */}
-        <div className="lg:hidden px-4 pb-12">
+        <div className="lg:hidden px-3 sm:px-4 pb-8 sm:pb-10 md:pb-12">
           <div className="max-w-sm mx-auto">
             <SidebarProfile />
           </div>
         </div>
 
         {/* Desktop & Mobile Content Wrapper */}
-        <div className="max-w-7xl mx-auto px-4 lg:px-8">
-          <div className="flex flex-col lg:flex-row gap-8 lg:gap-16 xl:gap-20">
+        <div className="max-w-7xl mx-auto px-3 sm:px-4 md:px-6 lg:px-8">
+          <div className="flex flex-col lg:flex-row gap-6 sm:gap-8 lg:gap-12 xl:gap-16 2xl:gap-20">
             
             {/* Desktop: Sticky sidebar on left */}
-            <aside className="hidden lg:block lg:w-80 xl:w-96 shrink-0">
+            <aside className="hidden lg:block lg:w-72 xl:w-80 2xl:w-96 shrink-0">
               <div className="sticky top-24">
                 <SidebarProfile />
               </div>
             </aside>
 
             {/* Main Content */}
-            <div className="flex-1 min-w-0 space-y-20 md:space-y-28 pb-16">
+            <div className="flex-1 min-w-0 space-y-12 sm:space-y-16 md:space-y-20 lg:space-y-24 xl:space-y-28 pb-12 sm:pb-14 md:pb-16">
               <AboutSection />
               <ProjectsSection />
               <ExperienceSection />
